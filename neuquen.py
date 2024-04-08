5d8fb14e081aae9e1583be0af79283a334abbe46
Malena2317 <me.navarro@alumno.um.edu.ar>
2024-04-08 14:55:02 -0300

    actualizacion de palabras palindromas con neuquen

--- neuquen.py
+++ neuquen.py
@@ -1,17 +1,17 @@
 
 import unittest
 
-
-def is_palindrome(neuquen):
-    return neuquen == neuquen[::-1]
+def is_palindrome(word):
+    word = word.lower().replace(" ", "")
+    return word == word[::-1]
 
 class TestPalindrome(unittest.TestCase):
     def test_neuquen(self):
         resultado = is_palindrome('n euq uen')
         self.assertEqual(resultado, True)
-
+    
     def test_neunen(self):
-        resultado = is_palindrome('neunen') # si es 
+        resultado = is_palindrome('neuen') # pali
         self.assertEqual(resultado, True)
     
     def test_quenen  (self):
@@ -19,8 +19,8 @@ class TestPalindrome(unittest.TestCase):
         self.assertEqual(resultado, False)
     
     def test_neuqueneu(self):
-        resultado = is_palindrome('neuqueneu') #si es 
-        self.assertEqual(resultado, True)
+        resultado = is_palindrome('neuqueneu') #no  es 
+        self.assertEqual(resultado, False)
 
     def test_nen(self):
         resultado = is_palindrome('nen') #si es 
@@ -37,5 +37,8 @@ class TestPalindrome(unittest.TestCase):
     def test_ueneu (self):
         resultado = is_palindrome('ueneu ') #si es 
         self.assertEqual(resultado, True)
-
+        
 unittest.main()
+
+
+
